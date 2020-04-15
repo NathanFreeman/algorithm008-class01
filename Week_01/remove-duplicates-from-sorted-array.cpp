2b975c75bb288992f09f2e0e13aeaef95cb14822
NathanFreeman <1056159381@qq.com>
2020-04-15 21:12:50 +0800

    删除排序数组中的重复项

---
+++ Week_01/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int> &nums) {
+        if (nums.empty()) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+int main() {
+    int len;
+
+    Solution solution;
+    vector<int> num = {1,2,3,3,4,5};
+    len = solution.removeDuplicates(num);
+
+    cout << len << endl;
+    return 0;
+}
