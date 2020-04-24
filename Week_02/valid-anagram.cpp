0381aad15fdb3668d6d0e91af1ecf9c31691c023
NathanFreeman <1056159381@qq.com>
2020-04-24 22:46:00 +0800

    有效的字母异位词

--- Week_02/valid-anagram.cpp
+++ Week_02/valid-anagram.cpp
@@ -1,54 +1,24 @@
-#include <string>
 #include <unordered_map>
 
 using namespace std;
 
-class Solution_1 {
+class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        if (s.length() != t.length()) { return false; }
 
         unordered_map<char, int> counts;
         int n = s.length();
-
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             counts[s[i]]++;
             counts[t[i]]--;
         }
 
-        for (auto count :counts) {
+        for (auto count : counts) {
             if (count.second > 0) {
                 return false;
             }
         }
-
         return true;
     }
 };
-
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        int counts[26] = {0};
-
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            counts[s[i] - 'a']++;
-            counts[t[i] - 'a']--;
-        }
-
-        for (int count : counts) {
-            if(count != 0){
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
